18993b7caeadb64066dccb14228bce03f83a0f7c
idan <idandrori555@gmail.com>
2025-05-15 14:26:36 +0300

    Fixed background to dark

--- public/app.js
+++ public/app.js
@@ -57,7 +57,7 @@ function renderOutput(data, isGrades = false) {
   if (!Array.isArray(data)) {
     const pre = document.createElement("pre");
     pre.textContent = JSON.stringify(data, null, 2);
-    pre.className = "bg-light p-3";
+    pre.className = "bg-dark p-3";
     container.appendChild(pre);
     return;
   }
